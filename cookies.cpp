f2f1124ca92785c2b70eea158e6104e44cb1aed9
Sachin <sachinfnatic@gmail.com>
2025-09-06 15:47:45 +0530

    feat: remove cs2-menus dependency to support both plats

--- cookies.cpp
+++ cookies.cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#include "ctimer.h"
 #include "cookies.h"
 #include "metamod_oslink.h"
 #include "schemasystem/schemasystem.h"
 
+#include <iomanip>
+#include <sstream>
+
 cookies g_cookies;
 PLUGIN_EXPOSE(cookies, g_cookies);
 IVEngineServer2* engine = nullptr;
-CGameEntitySystem* g_pGameEntitySystem = nullptr;
-CEntitySystem* g_pEntitySystem = nullptr;
-CGlobalVars *gpGlobals = nullptr;
+
+float g_flUniversalTime;
+float g_flLastTickedTime;
+bool g_bHasTicked;
 
 IMySQLClient *g_pMysqlClient;
 ISQLiteClient *g_pSqliteClient;
 IMySQLConnection* g_pConnectionMysql;
 ISQLConnection* g_pConnectionSQLite;
 bool g_bSQLite = false;
-
-IPlayersApi* g_pPlayers;
-IUtilsApi* g_pUtils;
 
 CookiesApi* g_pCookiesApi = nullptr;
 ICookiesApi* g_pCookiesCore = nullptr;
@@ -26,15 +28,26 @@ std::map<std::string, std::string> g_mapCookies[64];
 
 CGameEntitySystem* GameEntitySystem()
 {
-	return g_pUtils->GetCGameEntitySystem();
+#ifdef WIN32
+	static int offset = 88;
+#else
+	static int offset = 80;
+#endif
+	return *reinterpret_cast<CGameEntitySystem**>((uintptr_t)(g_pGameResourceServiceServer)+offset);
 }
 
-void StartupServer()
+// Will return null between map end & new map startup, null check if necessary!
+CGlobalVars* GetGlobals()
 {
-	g_pGameEntitySystem = GameEntitySystem();
-	g_pEntitySystem = g_pUtils->GetCEntitySystem();
-	gpGlobals = g_pUtils->GetCGlobalVars();
+	return engine->GetServerGlobals();
 }
+
+SH_DECL_HOOK0_void(IServerGameDLL, GameServerSteamAPIActivated, SH_NOATTRIB, 0);
+SH_DECL_HOOK3_void(IServerGameDLL, GameFrame, SH_NOATTRIB, 0, bool, bool, bool);
+SH_DECL_HOOK5_void(IServerGameClients, ClientDisconnect, SH_NOATTRIB, 0, CPlayerSlot, ENetworkDisconnectionReason, const char *, uint64, const char *);
+SH_DECL_HOOK4_void(IServerGameClients, ClientPutInServer, SH_NOATTRIB, 0, CPlayerSlot, char const *, int, uint64);
+SH_DECL_HOOK6_void(IServerGameClients, OnClientConnected, SH_NOATTRIB, 0, CPlayerSlot, const char*, uint64, const char *, const char *, bool);
+SH_DECL_HOOK6(IServerGameClients, ClientConnect, SH_NOATTRIB, 0, bool, CPlayerSlot, const char*, uint64, const char *, bool, CBufferString *);
 
 bool cookies::Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late)
 {
@@ -44,13 +57,154 @@ bool cookies::Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool 
 	GET_V_IFACE_ANY(GetEngineFactory, g_pSchemaSystem, ISchemaSystem, SCHEMASYSTEM_INTERFACE_VERSION);
 	GET_V_IFACE_CURRENT(GetEngineFactory, engine, IVEngineServer2, SOURCE2ENGINETOSERVER_INTERFACE_VERSION);
 	GET_V_IFACE_CURRENT(GetFileSystemFactory, g_pFullFileSystem, IFileSystem, FILESYSTEM_INTERFACE_VERSION);
+	GET_V_IFACE_ANY(GetServerFactory, g_pSource2Server, ISource2Server, SOURCE2SERVER_INTERFACE_VERSION);
+	GET_V_IFACE_ANY(GetServerFactory, g_pSource2GameClients, IServerGameClients, SOURCE2GAMECLIENTS_INTERFACE_VERSION);
+
+	SH_ADD_HOOK(IServerGameDLL, GameServerSteamAPIActivated, g_pSource2Server, SH_MEMBER(this, &cookies::OnGameServerSteamAPIActivated), false);
+	SH_ADD_HOOK(IServerGameDLL, GameFrame, g_pSource2Server, SH_MEMBER(this, &cookies::GameFrame), true);
+	SH_ADD_HOOK(IServerGameClients, ClientDisconnect, g_pSource2GameClients, SH_MEMBER(this, &cookies::OnClientDisconnect), true);
+	SH_ADD_HOOK(IServerGameClients, ClientConnect, g_pSource2GameClients, SH_MEMBER(this, &cookies::Hook_ClientConnect), false );
 
 	g_SMAPI->AddListener( this, this );
 
 	g_pCookiesApi = new CookiesApi();
 	g_pCookiesCore = g_pCookiesApi;
 
+	new CTimer(1.0f, []()
+	{
+		for(int i = 0; i < 64; i++)
+		{
+			if (!m_Players[i] || m_Players[i]->IsAuthenticated())
+				continue;
+
+			if(engine->IsClientFullyAuthenticated(CPlayerSlot(i)))
+			{
+				m_Players[i]->SetAuthenticated(true);
+				m_Players[i]->SetSteamId(m_Players[i]->GetUnauthenticatedSteamId());
+				OnClientAuthorized(i, m_Players[i]->GetUnauthenticatedSteamId64());
+				ConMsg("[%s] %i slot authenticated %lli\n", g_PLAPI->GetLogTag(), i, m_Players[i]->GetUnauthenticatedSteamId64());
+			}
+		}
+		return 1.0f;
+	});
+
 	return true;
+}
+
+bool cookies::Unload(char *error, size_t maxlen)
+{
+	ConVar_Unregister();
+
+	SH_REMOVE_HOOK(IServerGameDLL, GameServerSteamAPIActivated, g_pSource2Server, SH_MEMBER(this, &cookies::OnGameServerSteamAPIActivated), false);
+	SH_REMOVE_HOOK(IServerGameDLL, GameFrame, g_pSource2Server, SH_MEMBER(this, &cookies::GameFrame), true);
+	SH_REMOVE_HOOK(IServerGameClients, ClientDisconnect, g_pSource2GameClients, SH_MEMBER(this, &cookies::OnClientDisconnect), true);
+	SH_REMOVE_HOOK(IServerGameClients, ClientConnect, g_pSource2GameClients, SH_MEMBER(this, &cookies::Hook_ClientConnect), false );
+
+	return true;
+}
+
+void cookies::OnGameServerSteamAPIActivated()
+{
+	m_CallbackValidateAuthTicketResponse.Register(this, &cookies::OnValidateAuthTicketHook);
+}
+
+bool cookies::Hook_ClientConnect( CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, bool unk1, CBufferString *pRejectReason )
+{
+	Player *pPlayer = new Player(slot.Get());
+	pPlayer->SetUnauthenticatedSteamId(new CSteamID(xuid));
+	m_Players[slot.Get()] = pPlayer;
+	RETURN_META_VALUE(MRES_IGNORED, true);
+}
+
+void cookies::OnClientDisconnect( CPlayerSlot slot, ENetworkDisconnectionReason reason, const char *pszName, uint64 xuid, const char *pszNetworkID )
+{
+	int iSlot = slot.Get();
+
+	delete m_Players[iSlot];
+	m_Players[iSlot] = nullptr;
+}
+
+int g_iDelayAuthFailKick = 30;
+void cookies::OnValidateAuthTicketHook(ValidateAuthTicketResponse_t *pResponse)
+{
+	uint64 iSteamId = pResponse->m_SteamID.ConvertToUint64();
+	for (int i = 0; i < 64; i++)
+	{
+		if (!m_Players[i] || !(m_Players[i]->GetUnauthenticatedSteamId64() == iSteamId))
+			continue;
+		switch (pResponse->m_eAuthSessionResponse)
+		{
+			case k_EAuthSessionResponseOK:
+			{
+				if(m_Players[i]->IsAuthenticated())
+					return;
+				m_Players[i]->SetAuthenticated(true);
+				m_Players[i]->SetSteamId(m_Players[i]->GetUnauthenticatedSteamId());
+				OnClientAuthorized(i, m_Players[i]->GetUnauthenticatedSteamId64());
+				ConMsg("[%s] %i slot authenticated %lli\n", g_PLAPI->GetLogTag(), i, m_Players[i]->GetUnauthenticatedSteamId64());
+				return;
+			}
+
+			case k_EAuthSessionResponseAuthTicketInvalid:
+			case k_EAuthSessionResponseAuthTicketInvalidAlreadyUsed:
+			{
+				if (!g_iDelayAuthFailKick)
+					return;
+
+				// g_pUtilsApi->PrintToChat(i, g_vecPhrases["AuthTicketInvalid"].c_str());
+				[[fallthrough]];
+			}
+
+			default:
+			{
+				if (!g_iDelayAuthFailKick)
+					return;
+
+				// g_pUtilsApi->PrintToChat(i, g_vecPhrases["AuthFailed"].c_str(), g_iDelayAuthFailKick);
+
+				new CTimer(g_iDelayAuthFailKick, [i]()
+				{
+					// engine->DisconnectClient(i, NETWORK_DISCONNECT_KICKED_NOSTEAMLOGIN);
+					return -1.f;
+				});
+			}
+		}
+	}
+}
+
+void cookies::GameFrame(bool simulating, bool bFirstTick, bool bLastTick)
+{
+	if(!GetGlobals()) return;
+
+	if (simulating && g_bHasTicked)
+	{
+		g_flUniversalTime += GetGlobals()->curtime - g_flLastTickedTime;
+	}
+
+	g_flLastTickedTime = GetGlobals()->curtime;
+	g_bHasTicked = true;
+
+	for (int i = g_timers.Count() - 1; i >= 0; i--)
+	{
+		auto timer = g_timers[i];
+
+		if (timer->m_flLastExecute == -1)
+			timer->m_flLastExecute = g_flUniversalTime;
+
+		// Timer execute 
+		if (timer->m_flLastExecute + timer->m_flInterval <= g_flUniversalTime)
+		{
+			if (!timer->Execute())
+			{
+				delete timer;
+				g_timers.Remove(i);
+			}
+			else
+			{
+				timer->m_flLastExecute = g_flUniversalTime;
+			}
+		}
+	}
 }
 
 void* cookies::OnMetamodQuery(const char* iface, int* ret)
@@ -65,17 +219,10 @@ void* cookies::OnMetamodQuery(const char* iface, int* ret)
 	return nullptr;
 }
 
-bool cookies::Unload(char *error, size_t maxlen)
-{
-	ConVar_Unregister();
-	
-	return true;
-}
-
 void LoadConfig() {
 	KeyValues* pKVConfig = new KeyValues("Databases");
 	if (!pKVConfig->LoadFromFile(g_pFullFileSystem, "addons/configs/databases.cfg")) {
-		g_pUtils->ErrorLog("[%s] Failed to load databases config 'addons/config/databases.cfg'", g_PLAPI->GetLogTag());
+		ErrorLog("[%s] Failed to load databases config 'addons/config/databases.cfg'", g_PLAPI->GetLogTag());
 		return;
 	}
 	g_bSQLite = false;
@@ -172,35 +319,16 @@ void cookies::AllPluginsLoaded()
 {
 	char error[64];
 	int ret;
-	g_pUtils = (IUtilsApi *)g_SMAPI->MetaFactory(Utils_INTERFACE, &ret, NULL);
-	if (ret == META_IFACE_FAILED)
-	{
-		g_SMAPI->Format(error, sizeof(error), "Missing Utils system plugin");
-		ConColorMsg(Color(255, 0, 0, 255), "[%s] %s\n", GetLogTag(), error);
-		std::string sBuffer = "meta unload "+std::to_string(g_PLID);
-		engine->ServerCommand(sBuffer.c_str());
-		return;
-	}
-	g_pPlayers = (IPlayersApi *)g_SMAPI->MetaFactory(PLAYERS_INTERFACE, &ret, NULL);
-	if (ret == META_IFACE_FAILED)
-	{
-		g_pUtils->ErrorLog("[%s] Missing Players system plugin", g_PLAPI->GetLogTag());
-		std::string sBuffer = "meta unload "+std::to_string(g_PLID);
-		engine->ServerCommand(sBuffer.c_str());
-		return;
-	}
-	ISQLInterface* g_SqlInterface = (ISQLInterface *)g_SMAPI->MetaFactory(SQLMM_INTERFACE, &ret, nullptr);
+	ISQLInterface* pInterface = (ISQLInterface *)g_SMAPI->MetaFactory(SQLMM_INTERFACE, &ret, nullptr);
 	if (ret == META_IFACE_FAILED) {
-		g_pUtils->ErrorLog("[%s] Missing SQL plugin", g_PLAPI->GetLogTag());
+		ErrorLog("[%s] Missing SQL plugin", g_PLAPI->GetLogTag());
 		std::string sBuffer = "meta unload "+std::to_string(g_PLID);
 		engine->ServerCommand(sBuffer.c_str());
 		return;
 	}
-	g_pPlayers->HookOnClientAuthorized(g_PLID, OnClientAuthorized);
-	g_pMysqlClient = g_SqlInterface->GetMySQLClient();
-	g_pSqliteClient = g_SqlInterface->GetSQLiteClient();
+	g_pMysqlClient = pInterface->GetMySQLClient();
+	g_pSqliteClient = pInterface->GetSQLiteClient();
 	LoadConfig();
-	g_pUtils->StartupServer(g_PLID, StartupServer);
 }
 
 void CookiesApi::SetCookie(int iSlot, const char* sCookieName, const char* sData)
@@ -227,6 +355,45 @@ const char* CookiesApi::GetCookie(int iSlot, const char* sCookieName)
 	return "";
 }
 
+std::string formatCurrentTime() {
+    std::time_t currentTime = std::time(nullptr);
+    std::tm* localTime = std::localtime(&currentTime);
+    std::ostringstream formattedTime;
+    formattedTime << std::put_time(localTime, "%m/%d/%Y - %H:%M:%S");
+    return formattedTime.str();
+}
+
+std::string formatCurrentTime2() {
+    std::time_t currentTime = std::time(nullptr);
+    std::tm* localTime = std::localtime(&currentTime);
+    std::ostringstream formattedTime;
+    formattedTime << std::put_time(localTime, "error_%m-%d-%Y");
+    return formattedTime.str();
+}
+
+void ErrorLog(const char* msg, ...)
+{
+	va_list args;
+	va_start(args, msg);
+
+	char buf[1024];
+	V_vsnprintf(buf, sizeof(buf), msg, args);
+	va_end(args);
+
+	ConColorMsg(Color(255, 0, 0, 255), "[Error] %s\n", buf);
+
+	char szPath[256], szBuffer[2048];
+	g_SMAPI->PathFormat(szPath, sizeof(szPath), "%s/addons/logs/cookies-%s.txt", g_SMAPI->GetBaseDir(), formatCurrentTime2().c_str());
+	g_SMAPI->Format(szBuffer, sizeof(szBuffer), "L %s: %s\n", formatCurrentTime().c_str(), buf);
+
+	FILE* pFile = fopen(szPath, "a");
+	if (pFile)
+	{
+		fputs(szBuffer, pFile);
+		fclose(pFile);
+	}
+}
+
 ///////////////////////////////////////
 const char* cookies::GetLicense()
 {
@@ -235,7 +402,7 @@ const char* cookies::GetLicense()
 
 const char* cookies::GetVersion()
 {
-	return "1.1";
+	return "CR-1.1";
 }
 
 const char* cookies::GetDate()
@@ -250,7 +417,7 @@ const char *cookies::GetLogTag()
 
 const char* cookies::GetAuthor()
 {
-	return "Pisex";
+	return "Pisex, Cruze";
 }
 
 const char* cookies::GetDescription()
@@ -265,5 +432,5 @@ const char* cookies::GetName()
 
 const char* cookies::GetURL()
 {
-	return "https://discord.gg/g798xERK5Y";
+	return "https://discord.gg/g798xERK5Y | https://github.com/cruze03";
 }
--- cookies.h
+++ cookies.h
@@ -14,10 +14,13 @@
 #include <utlstring.h>
 #include <KeyValues.h>
 #include "CCSPlayerController.h"
-#include "include/menus.h"
 #include "include/mysql_mm.h"
 #include "include/sqlite_mm.h"
 #include "include/cookies.h"
+
+#include "steam/isteamuser.h"
+#include "steam/steam_api_common.h"
+#include "steam/steamclientpublic.h"
 
 class cookies final : public ISmmPlugin, public IMetamodListener
 {
@@ -26,6 +29,14 @@ public:
 	bool Unload(char* error, size_t maxlen);
 	void AllPluginsLoaded();
 	void* OnMetamodQuery(const char* iface, int* ret);
+
+	void OnClientDisconnect( CPlayerSlot slot, ENetworkDisconnectionReason reason, const char *pszName, uint64 xuid, const char *pszNetworkID );
+	void OnGameServerSteamAPIActivated();
+	void GameFrame(bool simulating, bool bFirstTick, bool bLastTick);
+	void OnValidateAuthTicketHook(ValidateAuthTicketResponse_t *pResponse);
+	bool Hook_ClientConnect( CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, bool unk1, CBufferString *pRejectReason );
+
+	STEAM_GAMESERVER_CALLBACK_MANUAL(cookies, OnValidateAuthTicket, ValidateAuthTicketResponse_t, m_CallbackValidateAuthTicketResponse);
 private:
 	const char* GetAuthor();
 	const char* GetName();
@@ -55,5 +66,39 @@ public:
 		}
 	}
 };
+
+class Player
+{
+public:
+	Player(int iSlot) : m_iSlot(iSlot) {
+		m_bAuthenticated = false;
+		m_SteamID = nullptr;
+	}
+	bool IsAuthenticated() { return m_bAuthenticated; }
+
+	uint64 GetUnauthenticatedSteamId64() { return m_UnauthenticatedSteamID->ConvertToUint64(); }
+	const CSteamID* GetUnauthenticatedSteamId() { return m_UnauthenticatedSteamID; }
+
+	uint64 GetSteamId64() { return m_SteamID?m_SteamID->ConvertToUint64():0; }
+	const CSteamID* GetSteamId() { return m_SteamID; }
+
+	void SetAuthenticated(bool bAuthenticated) { m_bAuthenticated = bAuthenticated; }
+
+	void SetUnauthenticatedSteamId(const CSteamID* steamID) { m_UnauthenticatedSteamID = steamID; }
+
+	void SetSteamId(const CSteamID* steamID) { m_SteamID = steamID; }
+private:
+	int m_iSlot;
+	bool m_bAuthenticated = false;
+	const CSteamID* m_UnauthenticatedSteamID;
+	const CSteamID* m_SteamID;
+};
+
+Player* m_Players[64];
+
+void OnClientAuthorized(int iSlot, uint64 iSteamID64);
+std::string formatCurrentTime();
+std::string formatCurrentTime2();
+void ErrorLog(const char* msg, ...);
 
 #endif //_INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
